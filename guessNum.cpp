76b267d76d1ad489f14c2136562ec9450c51bfdd
LuuNghiep333 <231561169+LuuNghiep333@users.noreply.github.com>
2025-09-10 16:21:48 -0700

    Create guessNum.cpp

---
+++ guessNum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int guess, random = 0;
+    bool isFound = false;
+    while(!isFound) {
+        random = rand()%100;
+        cout<<"Guess my favorite number!\nRange from 0 to 100"<<endl;
+        cout<<"Number: ";
+        cin >> guess;
+        if (guess > 0) {
+            if (guess != random)
+                cout<<"\nOops, It's "<<random<<", Try Again!"<<endl;
+            else {
+                isFound = true;
+                cout<<"\nCongratulations, You're beat my game :D";
+            }
+        }
+        else {
+            cout<<"\nUhh, "<<guess<<"?!\nYou ain't funny..."<<endl;
+            break;
+        }
+    }
+    return 0;
+}
